bfa581b65c11c48b05489689ec33e5689c54d46d
Aidan Maddox <gennaro.maddox@gmail.com>
2018-04-27 19:10:24 -0700

    Update

---
+++ animation_oac.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+$("#img1").delay(3000).animate({
+
+   opacity: '0',
+
+},4000);
+$("#img2").delay(9000).animate({
+
+   opacity: '0',
+
+},4000);
+
+//$("#img3").delay(2000);
+
+$("#img3").delay(15000).animate({
+
+   opacity: '0',
+
+},4000);
+
+//$("#img4").delay(2000);
+
+$("#img4").delay(21000).animate({
+
+   opacity: '0',
+
+},4000);
+
+//$("#img5").delay(2000);
+
+$("#img5").delay(27000).animate({
+
+   opacity: '0',
+
+},4000);
+
+})
