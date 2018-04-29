76bed38090ea005443596a81df338d005b2be365
AnkitMukherjee <ankitmukherjee@outlook.com>
2018-04-28 18:22:41 -0700

    Add files via upload

--- animation_oac.js
+++ animation_oac.js
@@ -1,40 +1,39 @@
 $(document).ready(function(){
+	
+	$("#narration").trigger("play");
+	$("#img1").delay(3000).animate({
 
+	   opacity: '0',
 
+	},4000);
+	$("#img2").delay(9000).animate({
 
-$("#img1").delay(3000).animate({
+	   opacity: '0',
 
-   opacity: '0',
+	},4000);
 
-},4000);
-$("#img2").delay(9000).animate({
+	//$("#img3").delay(2000);
 
-   opacity: '0',
+	$("#img3").delay(15000).animate({
 
-},4000);
+	   opacity: '0',
 
-//$("#img3").delay(2000);
+	},4000);
 
-$("#img3").delay(15000).animate({
+	//$("#img4").delay(2000);
 
-   opacity: '0',
+	$("#img4").delay(21000).animate({
 
-},4000);
+	   opacity: '0',
 
-//$("#img4").delay(2000);
+	},4000);
 
-$("#img4").delay(21000).animate({
+	//$("#img5").delay(2000);
 
-   opacity: '0',
+	$("#img5").delay(27000).animate({
 
-},4000);
+	   opacity: '0',
 
-//$("#img5").delay(2000);
-
-$("#img5").delay(27000).animate({
-
-   opacity: '0',
-
-},4000);
+	},4000);
 
 })
