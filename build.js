c2e551bbd375ee284c942614a0c7f9491771a329
Aidan Maddox <gennaro.maddox@gmail.com>
2018-04-28 17:17:40 -0700

    Update

--- build.js
+++ build.js
@@ -1,3 +1,38 @@
+var getCookie = function (cname)
+	{
+	var cookieLen = cname.length;
+	var cLen = document.cookie.length;
+	var i = 0;
+	var cEnd;
+
+	while (i < cLen)
+		{
+		var j = i + cookieLen;
+		if (document.cookie.substring(i,j) == cname)
+			{
+			cEnd = document.cookie.indexOf(";",j);
+
+			if (cEnd == -1)
+				{ cEnd = document.cookie.length; }
+
+			return unescape(document.cookie.substring((j+1), cEnd));
+			}
+		i++;
+		}
+
+	return " ";
+	}
+
+
+var var1;
+var var2;
+var name1;
+var name2;
+var prim;
+var sec;
+
+
+
 var colorsPrimary = new Array;
 colorsPrimary.push("Lava Rock");
 colorsPrimary.push("Matte Black");
@@ -11,12 +46,32 @@ colorsSecond.push("White Howlite");
 colorsSecond.push("Matte Amazonite");
 
 $(document).ready(function(){
-var1 = 0;
-var2 = 0;
-name1=0;
-name2=0;
-var prim = 0;
-var sec = 0;
+
+if(getCookie("Primary") == 0 && getCookie("Secondary") ==0 && getCookie("Var1") ==0 && getCookie("Var2") == 0 ){
+	var1 = 0;
+	var2  =0;
+	name1 = 0;
+	name2=0;
+	prim =0 ;
+	sec =0 ;
+
+
+
+}
+else{
+
+	var1 = getCookie("Var1");
+	var2  =getCookie("Var2");
+	name1 = getCookie("name1");
+	name2=getCookie("name2");
+	prim =getCookie("Primary") ;
+	//alert("primary"+ prim);
+	sec =getCookie("Secondary");
+	//alert("secondary" +sec);
+
+}
+
+
 $("#close").on("click", function(){
   $("#receipt").css("visibility", "hidden");
 })
@@ -25,12 +80,14 @@ $("#lava").on("click",function(){
 var1=0;
 name1=0;
 prim=0;
+
 })
 $("#matteblack").on("click",function(){
     $("#mainIMG").attr("src","images/build/Matte Black Onyx_oac.jpg");
 var1=1;
 name1=1;
 prim=1;
+
 })
 
 $("#white").on("click",function(){
@@ -38,12 +95,14 @@ $("#white").on("click",function(){
 var1=0;
 name1=2;
 prim=2;
+
 })
 $("#amazonite").on("click",function(){
     $("#mainIMG").attr("src","images/build/amazonite_oac.jpg");
     var1=1;
     name1=3;
     prim = 3;
+
 })
 
 
@@ -53,12 +112,14 @@ var2=0;
 name2=0;
 sec = 0;
 
+
 })
 $("#matteblack2").on("click",function(){
     $("#secondaryIMG").attr("src","images/build/Matte Black Onyx_secondary_oac.png");
     var2=1;
     name2=1;
     sec = 1;
+
 })
 
 $("#white2").on("click",function(){
@@ -66,12 +127,14 @@ $("#white2").on("click",function(){
 var2=0;
 name2=2;
 sec = 2;
+
 })
 $("#amazonite2").on("click",function(){
     $("#secondaryIMG").attr("src","images/build/amazonite_secondary_oac.png");
     var2=1;
     name2=3;
     sec = 3;
+
 })
 
 
@@ -121,9 +184,200 @@ else if(name2 ==3){
 //part two email validation
 emailVal($("#emailtext").val(), prim, sec, var1, var2);
 
+document.cookie = "Primary=" + prim;
+document.cookie = "Secondary=" + sec;
+document.cookie = "Var1=" + var1;
+document.cookie = "Var2=" + var2;
+document.cookie = "name1=" + name1;
+document.cookie = "name2=" + name2;
+//alert(document.cookie);
+
+
 })
 
+var primState;
+var secState;
+var var1State;
+var var2State;
+var name1State;
+var name2State;
+
+
+
+
+
 }) //close ready function
+
+function check(){
+
+
+
+  //alert("test");
+  primState = getCookie("Primary");
+  secState = getCookie("Secondary");
+  var1State=getCookie("Var1");
+  var2State = getCookie("Var2");
+	name1State = getCookie("name1");
+	name2State = getCookie("name2");
+
+  //alert("Primary Cookie = " + primState +"Secondary= " + secState + "Var1= "+ var1State + " Var2= "+ var2State);
+
+    if(primState == 0){
+
+    $("#mainIMG").attr("src","images/build/Black Lava Rock_oac.jpg");
+    //alert("prim = 0");
+    var1=0;
+    name1=0;
+    prim=0;
+
+  }
+  if(primState == 1){
+
+    $("#mainIMG").attr("src","images/build/Matte Black Onyx_oac.jpg");
+  var1=1;
+  name1=1;
+  prim=1;
+
+  }
+  if(primState == 2){
+
+    $("#mainIMG").attr("src","images/build/WhiteHowlite_oac.jpg");
+    var1=0;
+    name1=2;
+    prim=2;
+
+}
+  if(primState == 3){
+
+  $("#mainIMG").attr("src","images/build/amazonite_oac.jpg");
+  var1=1;
+  name1=3;
+  prim = 3;
+
+  }
+
+  //------------------Secondary ----------
+
+  if(secState == 0){
+
+    $("#secondaryIMG").attr("src","images/build/Black Lava Rock_secondary_oac.png");
+    var2=0;
+    name2=0;
+    sec = 0;
+
+}
+if(secState == 1){
+
+  $("#secondaryIMG").attr("src","images/build/Matte Black Onyx_secondary_oac.png");
+  var2=1;
+  name2=1;
+  sec = 1;
+
+}
+if(secState == 2){
+
+	$("#secondaryIMG").attr("src","images/build/WhiteHowlite_secondary_oac.png");
+	var2=0;
+	name2=2;
+	sec = 2;
+
+}
+if(secState == 3){
+
+	$("#secondaryIMG").attr("src","images/build/amazonite_secondary_oac.png");
+	var2=1;
+	name2=3;
+	sec = 3;
+
+}
+
+if(var1State==0&&var2State==0){
+	//alert("cost 12");
+	$("#price").html("<h3>Price: $12</h3>");
+	//$("#price").append("<h3>$12</h3>")
+
+}
+else if(var1State==1&&var2State==1){
+	//alert("cost 14");
+	$("#price").html("<h3>Price: $14</h3>");
+//  $("#price").append("<h3>$42</h3>")
+}
+else{
+	//alert("cost 13");
+	$("#price").html("<h3>Price: $13</h3>");
+//  $("#price").append("<h3>$13</h3>")
+}
+
+
+
+
+// ------------- Names ---------------------
+
+if(name1State==0){
+  $("#price").append("<h4>Primary: Lava </h4>");
+	name1 =0;
+}
+else if(name1State ==1){
+  $("#price").append("<h4>Primary: Matte Black Onyx </h4>");
+	name1 =1;
+}
+else if(name1State ==2){
+  $("#price").append("<h4>Primary: White Howlite </h4>");
+	name1 =2;
+}
+else if(name1State ==3){
+  $("#price").append("<h4>Primary: Amazonite </h4>");
+	name1 =3;
+}
+
+if(name2State==0){
+  $("#price").append("<h4>Secondary: Lava </h4>");
+	name2 = 0;
+}
+else if(name2State ==1){
+  $("#price").append("<h4>Secondary: Matte Black Onyx </h4>");
+	name2 = 1;
+}
+else if(name2State ==2){
+  $("#price").append("<h4>Secondary: White Howlite </h4>");
+	name2 = 2;
+}
+else if(name2State ==3){
+  $("#price").append("<h4>Secondary: Amazonite </h4>");
+	name2 = 3;
+}
+
+
+
+}
+
+
+function refresh(){
+  if(var1==0&&var2==0){
+    //alert("cost 12");
+    $("#price").html("<h3>Price: $12</h3>");
+    //$("#price").append("<h3>$12</h3>")
+
+  }
+  else if(var1==1&&var2==1){
+    //alert("cost 14");
+    $("#price").html("<h3>Price: $14</h3>");
+  //  $("#price").append("<h3>$42</h3>")
+  }
+  else{
+    //alert("cost 13");
+    $("#price").html("<h3>Price: $13</h3>");
+  //  $("#price").append("<h3>$13</h3>")
+  }
+
+
+}
+
+
+
+
+
+
 
 var emailVal = function(email, primary1, secondary1, var1, var2){
   if(email.indexOf("@") == -1 || email.indexOf(".")== -1){
@@ -141,7 +395,7 @@ var emailVal = function(email, primary1, secondary1, var1, var2){
      if(var1==0&&var2==0){
 
       totalAmount = "Total: $12";
-  
+
     }
     else if(var1==1&&var2==1){
 
